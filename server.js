2993635915c6b2be2f6261168798ec8463bd88a4
zhamby1 <zhamby1@ivytech.edu>
2022-04-13 10:18:34 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+var cors = require("cors")
+const morgan = require("morgan")
+const Song = require("./models/songs")
+
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.json())
+app.use(morgan('combined'))
+
+
+const router = express.Router();
+
+
+
+app.get('/', (req,res) =>{
+    res.send('Hello from my server')
+})
+
+router.get("/songs", (req,res) => {
+    Song.find((err,songs) =>{
+        if(err){
+            res.status(400).send(err)
+        }
+        else{
+            res.json(songs)
+        }
+    })
+
+})
+
+router.post("/songs", (req,res) => {
+    const song = new Song(req.body)
+    song.save((err,song) =>{
+        if(err){
+            res.status(400).send(err)
+        }
+        else{
+            res.status(200).json(song)
+        }
+    })
+})
+
+app.use("/api",router)
+
+app.listen(3000)

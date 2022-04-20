8390a4e0ed1373260c711065261b601a29cafe80
zhamby1 <zhamby1@ivytech.edu>
2022-04-20 10:33:12 -0400

    added update and details route

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/', (req,res) =>{
     res.send('Hello from my server')
 })
 
+//grab all songs
 router.get("/songs", (req,res) => {
     Song.find((err,songs) =>{
         if(err){
@@ -31,6 +32,7 @@ router.get("/songs", (req,res) => {
 
 })
 
+//Add song to database
 router.post("/songs", (req,res) => {
     const song = new Song(req.body)
     song.save((err,song) =>{
@@ -43,6 +45,39 @@ router.post("/songs", (req,res) => {
     })
 })
 
+//Use the ID in the URL path to find a single song
+router.get("/songs/:id", function(req,res){
+    Song.findById(req.params.id, function(err,song){
+        if(err){
+            res.status(400).send(err);
+        }
+        else{
+            res.json(song);
+        }
+    });
+});
+
+//update or edit a song
+
+router.put("/songs/:id", function(req, res) {
+    // Song to update sent in body of request
+    const song = req.body;
+ 
+    // Replace existing song fields with updated song
+    Song.updateOne({ _id: req.params.id }, song, function(err, result) {
+       if (err) {
+          res.status(400).send(err);
+       } 
+       else if (result.n === 0) {
+           res.sendStatus(404);
+       } 
+       else {
+           res.sendStatus(204);
+       }
+    });
+ });
+
+//delete a song
 router.delete("/songs/:id",(req,res) =>{
     Song.deleteOne({_id: req.params.id}, (err,result) =>{
         if(err){

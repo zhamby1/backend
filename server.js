960c4a03bd25cd932b4f44446cb306865e6b3795
zhamby1 <zhamby1@ivytech.edu>
2022-04-25 10:37:59 -0400

    -add auth and adding a new user

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const bodyParser = require("body-parser")
 var cors = require("cors")
 const morgan = require("morgan")
 const Song = require("./models/songs")
+const jwt = require("jwt-simple")
+const User = require("./models/user")
+const { restart } = require("nodemon")
 
 
 const app = express();
@@ -12,6 +15,62 @@ app.use(morgan('combined'))
 
 
 const router = express.Router();
+const secret = "supersecret"
+
+//route creates a new username and password (aka a new User)
+router.post("/user", (req,res) =>{
+    //see if the request has a username and password...if not exit function
+    if(!req.body.username || !req.body.password){
+        res.status(400).json({error: "Missing username and password"})
+        return
+    }
+    //create constant of newUser equal to the data the request send from the front-end
+    const newUser = new User({
+        username: req.body.username,
+        password: req.body.password,
+        status: req.body.status
+    })
+    //save the user to the database.  if errors, send status along with err message..otherwise send ok status
+    newUser.save(function(err){
+        if(err){
+            res.status(400).send(err)
+        }
+        else{
+            res.sendStatus(201)
+        }
+    })
+
+})
+
+router.post("/auth", function(req, res) {
+
+    if (!req.body.username || !req.body.password) {
+       res.status(401).json({ error: "Missing username and/or password"});
+       return;
+    }
+ 
+    // Get user from the database
+    User.findOne({ username: req.body.username }, function(err, user) {
+       if (err) {
+          res.status(400).send(err);
+       }
+       else if (!user) {
+          // Username not in the database
+          res.status(401).json({ error: "Bad username"});
+       }
+       else {
+          // Check if password from database matches given password
+          if (user.password != req.body.password) {
+             res.status(401).json({ error: "Bad password"});
+          }
+          else {
+             // Send back a token that contains the user's username
+             const token = jwt.encode({ username: user.username }, secret);
+             res.json({ token: token });
+          }
+       }
+    });
+ });
 
 
 

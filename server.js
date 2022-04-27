b201d643da016798dc05e6df096eb2c42ab73f35
zgumby85 <zhamby1@ivytech.edu>
2022-04-27 15:40:10 -0400

    added status and login

--- server.js
+++ server.js
@@ -1,124 +1,156 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-var cors = require("cors")
-const morgan = require("morgan")
-const Song = require("./models/songs")
-const jwt = require("jwt-simple")
+const express = require("express");
+const bodyParser = require('body-parser');
+const Song = require("./models/song");
+var cors = require('cors');
+const jwt = require("jwt-simple");
 const User = require("./models/user")
-const { restart } = require("nodemon")
-
 
 const app = express();
-app.use(cors())
-app.use(bodyParser.json())
-app.use(morgan('combined'))
+app.use(cors());
 
-
+app.use(bodyParser.json());
 const router = express.Router();
+
 const secret = "supersecret"
 
-//route creates a new username and password (aka a new User)
-router.post("/user", (req,res) =>{
-    //see if the request has a username and password...if not exit function
-    if(!req.body.username || !req.body.password){
-        res.status(400).json({error: "Missing username and password"})
-        return
-    }
-    //create constant of newUser equal to the data the request send from the front-end
-    const newUser = new User({
-        username: req.body.username,
-        password: req.body.password,
-        status: req.body.status
-    })
-    //save the user to the database.  if errors, send status along with err message..otherwise send ok status
-    newUser.save(function(err){
-        if(err){
-            res.status(400).send(err)
-        }
-        else{
-            res.sendStatus(201)
-        }
-    })
-
-})
-
-router.post("/auth", function(req, res) {
+//create a new username and password
+router.post("/user", function(req, res) {
 
     if (!req.body.username || !req.body.password) {
-       res.status(401).json({ error: "Missing username and/or password"});
+       res.status(400).json({ error: "Missing username and/or password"});
        return;
     }
  
-    // Get user from the database
-    User.findOne({ username: req.body.username }, function(err, user) {
+    const newUser = new User({
+       username: req.body.username,
+       password: req.body.password,
+       status:   req.body.status
+    });
+ 
+    newUser.save(function(err) {
        if (err) {
           res.status(400).send(err);
        }
-       else if (!user) {
-          // Username not in the database
-          res.status(401).json({ error: "Bad username"});
-       }
        else {
-          // Check if password from database matches given password
-          if (user.password != req.body.password) {
-             res.status(401).json({ error: "Bad password"});
-          }
-          else {
-             // Send back a token that contains the user's username
-             const token = jwt.encode({ username: user.username }, secret);
-             res.json({ token: token });
-          }
+          res.sendStatus(201);  // Created
        }
     });
  });
 
 
+//authentication
+router.post("/auth", function(req,res){
+    if (!req.body.username || !req.body.password) {
+        res.status(400).json({ error: "Missing username and/or password"});
+        return;
+     }
 
-app.get('/', (req,res) =>{
-    res.send('Hello from my server')
-})
-
-//grab all songs
-router.get("/songs", (req,res) => {
-    Song.find((err,songs) =>{
+    User.findOne({username: req.body.username}, function(err,user){
+        //connection or server err
         if(err){
             res.status(400).send(err)
         }
+        //checks to see if user is in database
+        else if(!user){
+            res.status(401).json({error:"Bad Username"})
+        }
+        //checks to see is the users password is correct
         else{
-            res.json(songs)
+            if(user.password != req.body.password){
+                res.status(401).json({error:"Bad Password"})
+            }
+            //succesful login
+            else{
+                //create a token that is encoded with jwt that sends back the username encoded
+                username2 = user.username
+                const token = jwt.encode({username: user.username},secret)
+
+                //respond with a token with the value of the token constant above
+                res.json({
+                    username2,
+                    token:token})
+
+            }
         }
     })
 
+
 })
 
-//Add song to database
-router.post("/songs", (req,res) => {
-    const song = new Song(req.body)
-    song.save((err,song) =>{
-        if(err){
-            res.status(400).send(err)
+//get status of users with a valid token currently, and see if it matches the clients token trying to access the application
+router.get("/status", function(req,res){
+    //see if the X-auth header is set
+    if(!req.headers["x-auth"]){
+        return res.status(401).json({error: "Missing X-Auth header"})
+    }
+
+    //if x-auth contains the token (it should)
+    const token = req.headers["x-auth"]
+    try{
+        const decoded = jwt.decode(token,secret)
+    
+
+    //send back all username and status fields that have tokens
+    User.find({},"username status", function(err,users){
+        res.json(users)
+    })
+}
+    catch(ex){
+        res.status(401).json({error: "invalid jwt"})
+    }
+
+})
+
+
+
+//Grab all songs in the database
+router.get("/songs", function(req,res){
+
+    let query = {};
+    if(req.query.genre){
+        query = {genre: req.query.genre};
+    }
+
+
+    Song.find(query,function(err,songs){
+        if (err){
+            res.status(400).send(err);
         }
         else{
-            res.status(200).json(song)
+            res.json(songs);
         }
-    })
-})
 
-//Use the ID in the URL path to find a single song
-router.get("/songs/:id", function(req,res){
-    Song.findById(req.params.id, function(err,song){
+    });
+
+
+});
+
+//add a song to the database
+router.post("/songs", function(req,res){
+    const song = new Song(req.body);
+    song.save(function(err,song){
         if(err){
             res.status(400).send(err);
         }
         else{
-            res.json(song);
+            res.status(201).json(song)
         }
+    })
+})
+
+router.get("/songs/:id", function(req, res) {
+    // Use the ID in the URL path to find the song
+    Song.findById(req.params.id, function(err, song) {
+       if (err) {
+          res.status(400).send(err);
+       } 
+       else {
+          res.json(song);
+       }
     });
-});
+ });
 
-//update or edit a song
-
-router.put("/songs/:id", function(req, res) {
+ router.put("/:id", function(req, res) {
     // Song to update sent in body of request
     const song = req.body;
  
@@ -136,22 +168,19 @@ router.put("/songs/:id", function(req, res) {
     });
  });
 
-//delete a song
-router.delete("/songs/:id",(req,res) =>{
-    Song.deleteOne({_id: req.params.id}, (err,result) =>{
-        if(err){
-            res.status(400).send(err)
-        }
-        else if(result.n === 0){
-            res.sendStatus(404)
-        }
-        else{
-            res.sendStatus(204)
-        }
+ router.delete("/songs/:id", function(req, res) {
+    Song.deleteOne({ _id: req.params.id }, function(err, result) {
+       if (err) {
+          res.status(400).send(err);
+       } 
+       else if (result.n === 0) {
+          res.sendStatus(404);
+       } 
+       else {
+          res.sendStatus(204);
+       }
+    });
+ });
 
-    })
-})
-
-app.use("/api",router)
-
-app.listen(process.env.PORT || 3000)
+app.use("/api", router);
+app.listen(3000);
